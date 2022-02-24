9c9b81d1474004a141e7f8f46a018ded81930d4c
Nicolas <nicolasseroiska@gmail.com>
2022-02-24 08:16:38 -0300

    first_project fixed

--- activities/first_project/main.c
+++ activities/first_project/main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
-int main(void)
-=======
+
 void main(void)
->>>>>>> 346ed3362e719d378bf30c3502365612c2e25a55
 {
     char c = 'A';
     int i = 30;
     float f = 50.25;
-<<<<<<< HEAD
-    double d;
-=======
 
->>>>>>> 346ed3362e719d378bf30c3502365612c2e25a55
+    double d;
+
+
+
     printf("| Prints the following variables - char c, int i, float f:\n\n");
     printf("| Char: %c\n\n", c);
     printf("| Int: %d\n\n", i);
@@ -22,13 +19,12 @@ void main(void)
 
     printf("-----------------------------------------------------------\n");
     printf("Hello world!\n ");
-<<<<<<< HEAD
+
 
     printf("Please, enter a number (can be a float one): ");
     scanf("%lf", &d);
     printf("\n The number you entered was: %lf",d);
-=======
->>>>>>> 346ed3362e719d378bf30c3502365612c2e25a55
+
     if(1 < 2){
         printf("\nWe've entered the if's block:\n \n ");
 

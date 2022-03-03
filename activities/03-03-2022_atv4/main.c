44d7065fe6e1add75e8d3836d7877823ab1eadac
Nicolas <nicolasseroiska@gmail.com>
2022-03-03 09:12:58 -0300

    atv - commission

---
+++ activities/03-03-2022_atv4/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int salesman_code;
+    float sell_amount;
+    float commission_percentage;
+
+    printf("Please, enter the salesman code: ");
+    scanf("%d", &salesman_code);
+
+    printf("How much did you sell ($)?\n");
+    scanf("%f", &sell_amount);
+
+    printf("What's the sell percentage(%) you receive as commission?\n");
+    scanf("%f", &commission_percentage);
+
+    printf("The commission amount that %d salesman will receive is: %f", salesman_code, sell_amount*commission_percentage/100);
+
+
+    return 0;
+}
